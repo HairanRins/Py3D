2a86eff89ada4f8606aa4af0e7933e1a8307e162
H. Rino <159486391+HairanRins@users.noreply.github.com>
2024-02-09 07:13:06 +0300

    Update 3D.py

--- 3D.py
+++ 3D.py
@@ -1,1 +1,16 @@
 # Voici le code source du mini-projet
+# Importation de la bibliothèque 
+import turtle
+
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor("black")
+t.width(2)
+t.speed(15)
+
+col = ('white', 'pink', 'cyan')
+for i in range (300):
+    t.pencolor(col[i%3])
+    t.forward(i*4)
+    t.right(121)
